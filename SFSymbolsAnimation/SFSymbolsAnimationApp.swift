5f9227791170290d6291a17a2b6b899089131b04
mk812y <mk812y@gmail.com>
2023-09-28 21:35:49 +0200

    Initial Commit

---
+++ SFSymbolsAnimation/SFSymbolsAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolsAnimationApp.swift
+//  SFSymbolsAnimation
+//
+//  Created by Михаил Куприянов on 28.9.23..
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolsAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
